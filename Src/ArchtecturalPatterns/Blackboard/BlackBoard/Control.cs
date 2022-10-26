0d21041183587e78b275b82dd244ee1d90b5fd02
Basharat <basharat@live.com>
2022-10-26 12:35:53 +0500

    Add files via upload

---
+++ Src/ArchtecturalPatterns/Blackboard/BlackBoard/Control.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Blackboard
+{
+    public class Control
+    {
+        BlackBoard blackBoard = null;
+        
+        public Control(BlackBoard blackBoard)
+        {
+            this.blackBoard = blackBoard;
+        }
+
+        public void loop()
+        {
+            System.Console.WriteLine("Starting loop");
+            if (blackBoard == null)
+                throw new InvalidOperationException("blackboard is null");
+            this.nextSource();
+            System.Console.WriteLine("Loop ended");
+            
+        }
+
+        /// <summary>
+        /// Selects the next source of knowledge (knowledgeworker by inspecting the blackgoard)
+        /// </summary>
+        void nextSource()
+        {
+            // observers the blackboard
+            foreach (KeyValuePair<string, ControlData> value in this.blackBoard.inspect())
+            {                  
+                 foreach (KnowledgeWorker worker in this.blackBoard.knowledgeWorkers)
+                 {
+                     if (worker.executeCondition())
+                     {
+                         Console.WriteLine(string.Format("{0} Can Contribute",worker.getName()));                         
+                         worker.executeAction();
+                         worker.updateBlackboard();
+                     }
+                 }                        
+            }
+        }
+    }
+}
